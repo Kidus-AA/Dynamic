6cc504b4658964fcb1c7d86d4f13ff6039ff4497
Kidus Asmare Ayele <kidusaa2014@gmail.com>
2022-02-17 00:53:10 -0500

    Implementation of static home and shop pages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import NavBar from './components/NavBar'
+import Home from './components/Home'
+import Shop from './components/Shop'
+import Footer from './components/Footer'
+import store from './store'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { createContext } from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+export const UserContext = createContext();
+
+const App = () => {
+    return (
+        <>
+            <BrowserRouter>
+                <UserContext.Provider value={store}>
+                    <NavBar/>
+                    <Routes>
+                        <Route path="*" element={<Home/>} />
+                        <Route path="/shop" element={<Shop/>} />
+                    </Routes>
+                </UserContext.Provider>
+            <Footer/>
+            
+            </BrowserRouter>
+        </>
+    )
 }
 
-export default App;
+export default App
