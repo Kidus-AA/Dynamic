393e3e70ed8169bbb2a6a9ad5c05bbb9ce7ae8e6
Kidus Asmare Ayele <kidusaa2014@gmail.com>
2022-02-22 01:09:10 -0500

    Shop page added

--- src/components/ShopContent/ShopContent.js
+++ src/components/ShopContent/ShopContent.js
@@ -1,13 +1,43 @@
 import styles from './ShopContent.module.css'
+import shirt from '../../assets/shirt.png'
+
+import Card from '../Card'
+import { useEffect } from 'react'
 // default values should be fetched from the database when this component loads
 // takes in values passed in as props and sends requests to the database
 // fetches the results and puts them in an array and creates cards from those 
 // when the next page is clicked it fetches other variables
 const ShopContent = (props) => {
-    return (
-        <>
+    useEffect(() => {
+        const observer = new IntersectionObserver((entries, observer) => {
+            entries.forEach(entry => {
+              if (entry.isIntersecting) {
+                entry.target.classList.remove("fadeOut");
+              } else {
+                entry.target.classList.add("fadeOut");
+              }
+            })
+          }, {
+            root: document,
+            rootMargin: "0px",
+          })
+          const fadeNodes = document.querySelectorAll(".fadeNode");
+          fadeNodes.forEach(node => {
+            observer.observe(node);
+          })
+    }, [])
 
-        </>
+    return (
+        <div className={styles.container}>
+            <Card image={shirt} name={"Shirt"} price={"$49.99"}/>
+            <Card image={shirt} name={"Shirt"} price={"$49.99"}/>
+            <Card image={shirt} name={"Shirt"} price={"$49.99"}/>
+            <Card image={shirt} name={"Shirt"} price={"$49.99"}/>
+            <Card image={shirt} name={"Shirt"} price={"$49.99"}/>
+            <Card image={shirt} name={"Shirt"} price={"$49.99"}/>
+            <Card image={shirt} name={"Shirt"} price={"$49.99"}/>
+            <Card image={shirt} name={"Shirt"} price={"$49.99"}/>
+        </div>
     )
 }
 
